640e841f9b792622c35068a03cf2a46d83f9d8b8
Bapusaheb Patil <bapoo1998@gmail.com>
2017-12-13 11:35:56 +0530

    Setting up AppBar for team info

--- app/src/main/java/bapspatil/pantheon/ui/TeamActivity.java
+++ app/src/main/java/bapspatil/pantheon/ui/TeamActivity.java
@@ -1,9 +1,15 @@
 package bapspatil.pantheon.ui;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import org.aviran.cookiebar2.CookieBar;
 
@@ -26,6 +32,18 @@ public class TeamActivity extends AppCompatActivity {
     private TeamRecyclerViewAdapter techTeamAdapter;
 
     @BindView(R.id.tech_team_rv) RecyclerView techTeamRecyclerView;
+    @BindView(R.id.appbar) AppBarLayout appBar;
+    @BindView(R.id.collapsing_bar) CollapsingToolbarLayout collapsingToolbar;
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    private void appBarInit() {
+        setSupportActionBar(toolbar);
+        collapsingToolbar.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.white));
+        collapsingToolbar.setCollapsedTitleTextColor(ContextCompat.getColor(this, android.R.color.white));
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.levi);
+        collapsingToolbar.setCollapsedTitleTypeface(typeface);
+        collapsingToolbar.setExpandedTitleTypeface(typeface);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +51,22 @@ public class TeamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_team);
         ButterKnife.bind(this);
 
+        appBarInit();
+
+        recyclerViewInit();
+
+        fetchTeam();
+    }
+
+    private void recyclerViewInit() {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         techTeamRecyclerView.setLayoutManager(layoutManager);
 
         techTeamAdapter = new TeamRecyclerViewAdapter(this, techTeam);
         techTeamRecyclerView.setAdapter(techTeamAdapter);
+    }
 
+    private void fetchTeam() {
         RetrofitAPI retrofitAPI = RetrofitAPI.retrofit.create(RetrofitAPI.class);
         Call<TeamResponse> teamResponseCall = retrofitAPI.getTeam();
         teamResponseCall.enqueue(new Callback<TeamResponse>() {

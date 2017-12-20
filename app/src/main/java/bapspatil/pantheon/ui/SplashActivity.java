ba651f8ae8f315cb46f28ce893c2cd8fa0b6bb58
Bapusaheb Patil <bapoo1998@gmail.com>
2017-12-21 03:55:16 +0530

    Added SplashActivity and changed icon (work in progress)

---
+++ app/src/main/java/bapspatil/pantheon/ui/SplashActivity.java
@@ -0,0 +1,44 @@
+package bapspatil.pantheon.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Slide;
+import android.view.Gravity;
+import android.view.View;
+
+import bapspatil.pantheon.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        hideSystemUI();
+        Slide slide = new Slide(Gravity.TOP);
+        getWindow().setExitTransition(slide);
+        int SPLASH_TIME_OUT = 1000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeCustomAnimation(SplashActivity.this, android.R.anim.fade_in, android.R.anim.fade_out);
+                startActivity(i, options.toBundle());
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    private void hideSystemUI() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+}

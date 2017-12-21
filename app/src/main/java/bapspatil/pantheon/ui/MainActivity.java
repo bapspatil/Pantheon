28418d5cf1ce80b6cd41ce0fee5567fd028cd03b
Bapusaheb Patil <bapoo1998@gmail.com>
2017-12-21 10:56:06 +0530

    Successfully replaced BottomNavigationView with BottomBar

--- app/src/main/java/bapspatil/pantheon/ui/MainActivity.java
+++ app/src/main/java/bapspatil/pantheon/ui/MainActivity.java
@@ -1,11 +1,10 @@
 package bapspatil.pantheon.ui;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 import bapspatil.pantheon.R;
 import butterknife.BindView;
@@ -13,7 +12,7 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.bottom_nav_view) BottomNavigationView bottomNavigationView;
+    @BindView(R.id.bottom_nav_view) BottomBar bottomNavigationView;
     HomeFragment homeFragment;
 
     @Override
@@ -29,24 +28,21 @@ public class MainActivity extends AppCompatActivity {
                 .addToBackStack(null)
                 .commit();
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                handleBottomNavClicked(item);
-                return true;
+            public void onTabSelected(int tabId) {
+                handleBottomNavClicked(tabId);
             }
         });
     }
 
-    private void handleBottomNavClicked(MenuItem item) {
-        switch (item.getItemId()) {
+    private void handleBottomNavClicked(int item) {
+        switch (item) {
             case R.id.action_home:
                 setTheme(R.style.AppTheme_Home);
-                bottomNavigationView.setBackgroundColor(getResources().getColor(R.color.homePrimary));
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    getWindow().setStatusBarColor(getResources().getColor(R.color.homePrimaryDark));
-                    getWindow().setNavigationBarColor(getResources().getColor(R.color.homePrimaryDark));
-                }
+                getWindow().setStatusBarColor(getResources().getColor(R.color.homePrimaryDark));
+                getWindow().setNavigationBarColor(getResources().getColor(R.color.homePrimaryDark));
+
                 getSupportFragmentManager().beginTransaction()
                         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
                         .replace(R.id.main_container, homeFragment)
@@ -55,11 +51,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.action_updates:
                 setTheme(R.style.AppTheme_Updates);
-                bottomNavigationView.setBackgroundColor(getResources().getColor(R.color.updatesPrimary));
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    getWindow().setStatusBarColor(getResources().getColor(R.color.updatesPrimaryDark));
-                    getWindow().setNavigationBarColor(getResources().getColor(R.color.updatesPrimaryDark));
-                }
+                getWindow().setStatusBarColor(getResources().getColor(R.color.updatesPrimaryDark));
+                getWindow().setNavigationBarColor(getResources().getColor(R.color.updatesPrimaryDark));
 
                 getSupportFragmentManager().beginTransaction()
                         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
@@ -69,11 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.action_events:
                 setTheme(R.style.AppTheme_Events);
-                bottomNavigationView.setBackgroundColor(getResources().getColor(R.color.eventsPrimary));
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    getWindow().setStatusBarColor(getResources().getColor(R.color.eventsPrimaryDark));
-                    getWindow().setNavigationBarColor(getResources().getColor(R.color.eventsPrimaryDark));
-                }
+                getWindow().setStatusBarColor(getResources().getColor(R.color.eventsPrimaryDark));
+                getWindow().setNavigationBarColor(getResources().getColor(R.color.eventsPrimaryDark));
 
                 getSupportFragmentManager().beginTransaction()
                         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
@@ -83,11 +73,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.action_team:
                 setTheme(R.style.AppTheme_Team);
-                bottomNavigationView.setBackgroundColor(getResources().getColor(R.color.teamPrimary));
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    getWindow().setStatusBarColor(getResources().getColor(R.color.teamPrimaryDark));
-                    getWindow().setNavigationBarColor(getResources().getColor(R.color.teamPrimaryDark));
-                }
+                getWindow().setStatusBarColor(getResources().getColor(R.color.teamPrimaryDark));
+                getWindow().setNavigationBarColor(getResources().getColor(R.color.teamPrimaryDark));
 
                 getSupportFragmentManager().beginTransaction()
                         .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
